ac47a012d45d143bdc0501a4f14023097530e1b9
Michal Zlatkovský <michal@zlatkovsky.cz>
2022-02-02 13:46:21 +0100

    cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -246,104 +246,52 @@ rerollImage()
   .then(() => repaintImage());
 
 // /////////////////////
-// Log events flag
+const evCache = [];
+let prevDiff = -1;
 
-// Global vars to cache event state
-var evCache = [];
-var prevDiff = -1;
-
-function clearLog(event) {
-  var o = document.getElementsByTagName('output')[0];
-  o.innerHTML = "";
-}
-
-function pointerdown_handler(ev) {
-  console.log("pd")
-  // The pointerdown event signals the start of a touch interaction.
-  // This event is cached to support 2-finger gestures
-  evCache.push(ev);
-}
-
-function pointermove_handler(ev) {
-  // This function implements a 2-pointer horizontal pinch/zoom gesture. 
-  //
-  // If the distance between the two pointers has increased (zoom in), 
-  // the taget element's background is changed to "pink" and if the 
-  // distance is decreasing (zoom out), the color is changed to "lightblue".
-  //
-  // This function sets the target element's border to "dashed" to visually
-  // indicate the pointer's target received a move event.
-  ev.target.style.border = "dashed";
-  console.log(evCache)
-
-  // Find this event in the cache and update its record with this event
-  for (var i = 0; i < evCache.length; i++) {
-    if (ev.pointerId == evCache[i].pointerId) {
-      evCache[i] = ev;
+canvas.addEventListener("pointerdown", (e) => evCache.push(e));
+canvas.addEventListener("pointermove", (e) => {
+  for (let i = 0; i < evCache.length; i++) {
+    if (e.pointerId === evCache[i].pointerId) {
+      evCache[i] = e;
       break;
     }
   }
 
   // If two pointers are down, check for pinch gestures
-  if (evCache.length == 2) {
+  if (evCache.length === 2) {
     // Calculate the distance between the two pointers
-    var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
-    const slider = document.getElementById("slider");
+    const curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
 
     if (prevDiff > 0) {
+      // zoom in
       if (curDiff > prevDiff) {
-        // The distance between the two pointers has increased
-        console.log("Pinch moving OUT -> Zoom in", ev);
-        slider.value = Number(slider.value) + 1;
+        slider.value = Number(slider.value) + 2;
         moveSlider(slider.value);
       }
+      // zoom out
       if (curDiff < prevDiff) {
-        // The distance between the two pointers has decreased
-        console.log("Pinch moving IN -> Zoom out", ev);
-        slider.value = Number(slider.value) - 1;
+        slider.value = Number(slider.value) - 2;
         moveSlider(slider.value);
       }
     }
 
-    // Cache the distance for the next move event 
     prevDiff = curDiff;
   }
-}
+});
 
-function pointerup_handler(ev) {
-  console.log("pup")
-  // Remove this pointer from the cache and reset the target's
-  // background and border
-  remove_event(ev);
-  ev.target.style.background = "white";
-  ev.target.style.border = "1px solid black";
-
-  // If the number of pointers down is less than two then reset diff tracker
-  if (evCache.length < 2) prevDiff = -1;
-}
-
-function remove_event(ev) {
-  // Remove this event from the target's cache
-  for (var i = 0; i < evCache.length; i++) {
-    if (evCache[i].pointerId == ev.pointerId) {
+const onPointerUp = (e) => {
+  for (let i = 0; i < evCache.length; i++) {
+    if (evCache[i].pointerId === e.pointerId) {
       evCache.splice(i, 1);
       break;
     }
   }
-}
+  // If the number of pointers down is less than two then reset diff tracker
+  if (evCache.length < 2) prevDiff = -1;
+};
 
-function init() {
-  // Install event handlers for the pointer target
-  var el = canvas;
-  el.onpointerdown = pointerdown_handler;
-  el.onpointermove = pointermove_handler;
-
-  // Use same handler for pointer{up,cancel,out,leave} events since
-  // the semantics for these events - in this app - are the same.
-  el.onpointerup = pointerup_handler;
-  el.onpointercancel = pointerup_handler;
-  el.onpointerout = pointerup_handler;
-  el.onpointerleave = pointerup_handler;
-}
-init();
-// /////////////////////
+canvas.addEventListener("pointerup", onPointerUp);
+canvas.addEventListener("pointercancel", onPointerUp);
+canvas.addEventListener("pointerout", onPointerUp);
+canvas.addEventListener("pointerleave", onPointerUp);
